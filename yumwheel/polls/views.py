556566cc5a3f61fb8f9645e828a7c3f34d4b4cc7
mdant1 <mdant1@mail.csuchico.edu>
2017-11-12 22:25:38 -0800

    views and url for restaurant

--- yumwheel/polls/views.py
+++ yumwheel/polls/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 def index(request):
     context = {"variable":"CINS 465 Hello World"}
     return render(request,"default.html",context)
+
+def restaurant(request):
+    return render(request, "restaurant.html", context)

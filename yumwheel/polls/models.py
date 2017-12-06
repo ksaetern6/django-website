730b5b3dca6f00e9b00afcab3f5160017c3af003
ksaetern6 <ksaetern6@mail.csuchico.edu>
2017-12-06 01:44:41 -0800

    fixed templates from a bad pull and register page

--- yumwheel/polls/models.py
+++ yumwheel/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
-# Create your models here.
-#class UserProfile(models.Model)
-    #user = Models.OneToOneField(User)
+#Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=50, default='Full Name')
